3677ebc434e0eba51d57d1d727886ef247b1d30a
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-05 20:11:33 +0530

    draft posts feature

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)  #commit=False because we want to save only after adding author and publish_date to form
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -38,9 +38,14 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.published_date = None
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
