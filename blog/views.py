0c548e4d77d2fd1f0e06c2f815bdab042f9c9159
Vijay-Mundaragi <saivijay010@gmail.com>
2019-02-05 20:52:23 +0530

    deleting posts

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from .forms import PostForm
 
 # Create your views here.
 
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -50,7 +49,14 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
